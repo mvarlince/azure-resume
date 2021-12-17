bcebd57724f835d42e9a4bb7820a2a8e47056c80
Varlince <42895726+mvarlince@users.noreply.github.com>
2021-12-16 21:20:20 -0500

    Updates to structure, created main.js file

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,25 @@
-window.addEventListener('DOMContentLoaded',(event) =>{
+//Content is loaded > go and run the event getVisitCount
+window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+
+const functionApi = '';
+
+// Javascript code that will grab the JSON that is provided from the API
+// grab the correct part of that JSON and show it on HTM
+
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response =>{
+    fetch(functionApi).then(response => {
         return response.json()
-    }).then(response =>{
+    }).then(response => {
         console.log("Website called function API.");
         count = response.count;
-        document.getElementById("counter").innterText = count;
+        document.getElementById("counter").innerText= count;
     }).catch(function(error){
         console.log(error);
     });
-    return count
+    return count;
 }
