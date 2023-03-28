9a0498a4ebd5a3092f3dcd4fb57d1cdf9b9d8763
Varlince Meran <varlincemeran@gmail.com>
2023-03-28 18:38:21 -0400

    updated work history and added skills section

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,5 @@
 //Content is loaded > go and run the event getVisitCount
-window.addEventListener('DOMContentLoaded', (event) =>{
+window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
@@ -12,14 +12,15 @@ const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApiUrl).then(response => {
-        return response.json()
-    }).then(response => {
-        console.log("Website called function API.");
-        count = response.count;
-        document.getElementById("counter").innerText= count;
-    }).catch(function(error){
-        console.log(error);
-    });
+    fetch(functionApiUrl)
+        .then(response => {
+            return response.json()
+        }).then(response => {
+            console.log("Website called function API.");
+            count = response.count;
+            document.getElementById("counter").innerText = count;
+        }).catch(function (error) {
+            console.log(error);
+        });
     return count;
 }
